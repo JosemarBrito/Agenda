c7bd9fb4889415550f36079446c99b23553ec585
Josemar Brito <estudos.josemarbrito@gmail.com>
2022-08-12 19:47:58 -0300

    Incluido templates para inserção de eventos da agenda. Incluido o gitgnore para ignorar os arquivos compilados

--- agenda/urls.py
+++ agenda/urls.py
@@ -26,4 +26,6 @@ urlpatterns = [
     path('login/', views.login_user),
     path('login/submit', views.submit_login),
     path('logout/', views.logout_user),
+    path('agenda/evento/', views.evento),
+    path('agenda/evento/submit', views.submit_evento),
 ]
