7c0a4e73a7f9ee32a0b0939966428ae66ab56928
Josemar Brito <estudos.josemarbrito@gmail.com>
2022-08-12 21:52:37 -0300

    Adicionado funçôes de alteração e exclusão de dados
    close #5

--- agenda/urls.py
+++ agenda/urls.py
@@ -22,10 +22,13 @@ from cont import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('agenda/', views.lista_eventos),
+    path('agenda/evento/', views.evento),
+    path('agenda/evento/submit', views.submit_evento),
+    path('agenda/evento/delete/<int:id_evento>/', views.delete_evento),
     path('', RedirectView.as_view(url='/agenda/')),
     path('login/', views.login_user),
     path('login/submit', views.submit_login),
     path('logout/', views.logout_user),
-    path('agenda/evento/', views.evento),
-    path('agenda/evento/submit', views.submit_evento),
+
+
 ]
